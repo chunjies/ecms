97daba60d334739d0a12c99fec698ae64dd8dc52
大虫 <wei.chunjie@outlook.com>
2019-04-06 14:30:31 +0800

    结构修改

--- src/main/java/com/chunjies/config/WebConfig.java
+++ src/main/java/com/chunjies/config/WebConfig.java
@@ -1,4 +1,6 @@
 package com.chunjies.config;
 
-public class WebConfig {
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+public class WebConfig extends WebMvcConfigurationSupport {
 }
